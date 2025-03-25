84efdd0bfaa4c84e4bd02c64bd3ca885a25be933
Ronuel Reyes <80933439+Ronuel-R@users.noreply.github.com>
2025-03-25 13:04:53 +0800

    3/25/2025 Update Secret

--- controllers/mailController.js
+++ controllers/mailController.js
@@ -1,17 +1,27 @@
-const nodemailer = require('nodemailer');
-const { mailTransporter, mailOptions } = require('../config/mailConfig');
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+const { mailTransporter, mailOptions } = require("../config/mailConfig");
 
 exports.sendMail = async (req, res) => {
-    const { firstname, lastname, email, message } = req.body;
-
+  const { firstname, lastname, email, message } = req.body;
+  const secret = req.headers["secret-key"];
+  
+  if (process.env.SECRET_KEY === secret) {
     if (!firstname || !lastname || !email || !message) {
-        return res.status(400).json({ error: 'All fields are required' });
+      return res.status(400).json({ error: "All fields are required" });
     }
 
     try {
-        await mailTransporter.sendMail(mailOptions(firstname, lastname, email, message));
-        res.json({ message: `📩 Received a message from ${firstname}` });
+      await mailTransporter.sendMail(
+        mailOptions(firstname, lastname, email, message)
+      );
+      res.json({ message: `📩 Received a message from ${firstname}` });
     } catch (error) {
-        res.status(500).json({ error: 'Failed to send email', details: error.message });
+      res
+        .status(500)
+        .json({ error: "Failed to send email", details: error.message });
     }
+  } else {
+    res.status(403).json({ error: "Forbidden" });
+  }
 };
